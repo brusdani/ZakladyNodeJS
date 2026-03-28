7affc87db6248886f607f51c375bf7488ebec43e
brusdani <daniel.brus@seznam.cz>
2026-03-28 22:16:53 +0100

    Homework06 db schema and migration

--- homework06/todos2026-main/src/schema.js
+++ homework06/todos2026-main/src/schema.js
@@ -1,7 +1,12 @@
-import { sqliteTable, int, text } from 'drizzle-orm/sqlite-core'
+import {sqliteTable, int, text} from 'drizzle-orm/sqlite-core'
+
 
 export const todosTable = sqliteTable('todos', {
-  id: int().primaryKey({ autoIncrement: true }),
-  title: text().notNull(),
-  done: int({ mode: 'boolean' }).notNull(),
+    id: int().primaryKey({autoIncrement: true}),
+    title: text().notNull(),
+    done: int({mode: 'boolean'}).notNull(),
+    //Homework06 changes
+    priority: text('priority', { enum: ["low", "normal", "high"] })
+        .notNull()
+        .default('normal'),
 })
